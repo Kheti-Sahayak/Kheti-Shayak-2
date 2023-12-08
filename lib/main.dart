44e88713a85e8a6545cc4e0bb1621baf36bad13a
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-12-08 20:38:35 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kheti_shayak_my_version/screens/auth/login_signup_screen.dart';
 import 'package:kheti_shayak_my_version/utils/colors.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
