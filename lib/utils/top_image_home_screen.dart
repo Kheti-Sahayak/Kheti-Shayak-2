70e846ae51fee50b775963a2dec99968f27ce1d0
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-12-08 11:40:59 +0530

    Top Image Home Screen created

---
+++ lib/utils/top_image_home_screen.dart
@@ -0,0 +1,9 @@
+class TopImageHomeScreen {
+  static const List<String> topImageSrc = [
+    'assets/images/plant_care.jpg',
+    'assets/images/google.png',
+    'assets/images/plant_care.jpg',
+    'assets/images/facebook.png',
+    'assets/images/plant_care.jpg',
+  ];
+}
